280a17592aea46bdee621620399eefe8eb343e89
sijlver <sijlver@mail.ru>
2017-01-28 00:02:51 +0300

    guessing-game/feature: wrote guessing-game

--- src/guessing-game.js
+++ src/guessing-game.js
@@ -1,20 +1,34 @@
 class GuessingGame {
-    constructor() {}
+    constructor() {
+    	this.mid=null;
+    	this.min=null;
+    	this.max=null;
+        this.solve=false;
+    }
 
     setRange(min, max) {
-
+    	this.min=min;
+    	this.max=max;
     }
 
     guess() {
-
+    	this.mid=Math.ceil((this.min+this.max)/2);
+        this.solve=true;
+    	return this.mid;
     }
 
     lower() {
-
+        if(this.solve){
+    	   this.max=this.mid;
+           this.solve=false;
+        }
     }
 
     greater() {
-
+        if(this.solve){
+    	   this.min=this.mid;
+           this.solve=false;
+        }
     }
 }
 
